56a26d203683e3ce57b987bd69a1dad38ad9772b
Vojtech Cima <vojtech.cima@vsb.cz>
2020-08-20 15:06:20 +0200

    HEAppE <3 Python

---
+++ heappeac/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.cluster_information_api import ClusterInformationApi
+from swagger_client.api.file_transfer_api import FileTransferApi
+from swagger_client.api.job_management_api import JobManagementApi
+from swagger_client.api.job_reporting_api import JobReportingApi
+from swagger_client.api.user_and_limitation_management_api import UserAndLimitationManagementApi
