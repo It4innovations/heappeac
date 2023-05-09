0b90ea711bdb47ed4da6dabab531fe5ab804ee3d
Jan Křenek <jan.krenek@vsb.cz>
2023-05-09 13:24:35 +0200

    V4.0.0

--- example.py
+++ example.py
@@ -10,98 +10,131 @@ from scp import SCPClient
 
 import heappeac as hp
 
+configuration = hp.Configuration()
+configuration.host = ""
+project = ""
 username = ""
 password = ""
-
-configuration = hp.Configuration()
 api_instance = hp.ApiClient(configuration)
 
-print(f"Authenticating {username}...")
+
+print(f"Authenticating to HEAppE instance through {username}...")
 cred = {
     "_preload_content": False,
     "body": {
         "Credentials": {
-            "Password": password,
-            "Username": username
+            "Username": username,
+            "Password": password
         }
     }
 }
-ulm = hp.api.UserAndLimitationManagementApi(api_instance)
-r = ulm.heappe_user_and_limitation_management_authenticate_user_password_post(**cred)
+
+ulmEndpoint = hp.UserAndLimitationManagementApi(api_instance)
+r = ulmEndpoint.heappe_user_and_limitation_management_authenticate_user_password_post(**cred)
 session_code = json.loads(r.data)
 print(f"Session code: {session_code}")
 
 
-print("Fetching cluster info...")
+print("\nFetching cluster info...")
 lac_body = {
-    "_preload_content": False,
+    "_preload_content": False
 }
-ci = hp.api.ClusterInformationApi(api_instance)
-r = ci.heappe_cluster_information_list_available_clusters_get(**lac_body)
+
+ciEndpoint = hp.ClusterInformationApi(api_instance)
+r = ciEndpoint.heappe_cluster_information_list_available_clusters_get(**lac_body)
 r_data = json.loads(r.data)
-print(r_data)
+print(json.dumps(r_data, indent = 3))
 
 
-print("Creating job template...")
-jm = hp.api.JobManagementApi(api_instance)
+print("\nFetching available computational projects...")
+lproj_body = {
+    "_preload_content": False,
+    "session_code": session_code
+}
 
+r = ulmEndpoint.heappe_user_and_limitation_management_projects_for_current_user_get(**lproj_body)
+r_data = json.loads(r.data)
+project = next(f["Project"] for f in r_data if f["Project"]["AccountingString"] == project)
+print(json.dumps(project, indent = 3))
+
+
+print("\nCreating job template...")
 job_spec_body = {
     "_preload_content": False,
     "body": {
         "JobSpecification": {
-        "Name": "my_job",
-        #"Project": "test_project",
-        #"WaitingLimit": 0,
-        #"NotificationEmail": "string",
-        #"PhoneNumber": "string",
-        #"NotifyOnAbort": true,
-        #"NotifyOnFinish": true,
-        #"NotifyOnStart": true,
-        "ClusterId": 2,
-        "FileTransferMethodId": 2,
-        "EnvironmentVariables": [],
-        "Tasks": [
-            {
-                "Name": "task_1",
-                "MinCores": 1,
-                "MaxCores": 24,
-                "WalltimeLimit": 600,
-                #"RequiredNodes": "string",
-                "Priority": 4,
-                #"JobArrays": "string",
-                #"IsExclusive": true,
-                #"IsRerunnable": true,
-                #"StandardInputFile": "string",
-                "StandardOutputFile": "stdout",
-                "StandardErrorFile": "stderr",
-                "ProgressFile": "stdprog",
-                "LogFile": "stdlog",
-                #"ClusterTaskSubdirectory": "string",
-                "ClusterNodeTypeId": 8,
-                "CommandTemplateId": 2,
-                #"CpuHyperThreading": true,
-                "EnvironmentVariables": [],
-                #"DependsOn": [],
-                "TemplateParameterValues": [
-                    {
-                        "CommandParameterIdentifier": "inputParam",
-                        "ParameterValue": "test"
-                    }
-                ]
-            }
-        ]
+            "Name": "job_1",
+            #"NotificationEmail": "string",
+            #"PhoneNumber": "string",
+            #"NotifyOnAbort": true,
+            #"NotifyOnFinish": true,
+            #"NotifyOnStart": true,
+            "ClusterId": 2,
+            "FileTransferMethodId": 2,
+            "ProjectId": project["Id"],
+            #"WaitingLimit": 0,
+            #"IsExtraLong": true,
+            "EnvironmentVariables": [],
+            "Tasks": [
+                {
+                    "Name": "task_1",
+                    "MinCores": 1,
+                    "MaxCores": 128,
+                    "Priority": 4,
+                    "WalltimeLimit": 600,                  
+                    #"PlacementPolicy": "string"
+                    #"RequiredNodes": [
+                    #   "string"
+                    # ],
+                    #"ClusterTaskSubdirectory": "string",
+                    #"CpuHyperThreading": true,
+                    #"JobArrays": "string",
+                    #"IsExclusive": true,
+                    #"IsRerunnable": true,
+                    #"StandardInputFile": "stdin",
+                    "StandardOutputFile": "stdout",
+                    "StandardErrorFile": "stderr",
+                    "ProgressFile": "stdprog",
+                    "LogFile": "stdlog",
+                    "ClusterNodeTypeId": 8,
+                    "CommandTemplateId": 3,
+                    #"TaskParalizationParameters": [
+                        #{
+                            #"MPIProcesses": 0,
+                            #"OpenMPThreads": 0,
+                            #"MaxCores": 0
+                        #}
+                    #],
+                    #"EnvironmentVariables": [
+                        #{
+                            #"Name": "string"
+                            #"Value": "string"
+                        #}
+                    #],
+                    #"DependsOn": [
+                    #   "string"
+                    # ],
+                    "TemplateParameterValues": [
+                        {
+                            "CommandParameterIdentifier": "inputParam",
+                            "ParameterValue": "testValue"
+                        }
+                    ]
+                }
+            ]
         },
         "SessionCode": session_code
     }
 }
-r = jm.heappe_job_management_create_job_post(**job_spec_body)
+
+jmEndpoint = hp.JobManagementApi(api_instance)
+r = jmEndpoint.heappe_job_management_create_job_post(**job_spec_body)
 r_data = json.loads(r.data)
 job_id = r_data["Id"]
-tasks = r_data["Tasks"]
-print(f"Job ID: {job_id}")
+print(f"Job ID: {job_id} ...")
 
 
-print(f"Submitting job {job_id}...")
+print(f"\nSubmitting job {job_id}...")
 submit_body = {
     "_preload_content": False,
     "body":
@@ -110,34 +143,31 @@ submit_body = {
             "SessionCode": session_code
         }
 }
-r = jm.heappe_job_management_submit_job_post(**submit_body)
+
+r = jmEndpoint.heappe_job_management_submit_job_put(**submit_body)
 r_data = json.loads(r.data)
 
 
-print(f"Waiting for job {job_id} to finish...")
+print(f"\nWaiting for job {job_id} to finish...")
 gcji_body = {
     "_preload_content": False,
-    "body": {
-        "SubmittedJobInfoId": job_id,
-        "SessionCode": session_code
-    }
+    "session_code": session_code,
+    "submitted_job_info_id": job_id
 }
 
 while True:
-    r = jm.heappe_job_management_get_current_info_for_job_post(**gcji_body)
+    r = jmEndpoint.heappe_job_management_current_info_for_job_get(**gcji_body)
     r_data = json.loads(r.data)
     state = r_data["State"]
-    # State codes: https://code.it4i.cz/ADAS/HEAppE/Middleware/-/blob/new_master/ServiceTier/EtchProxy
-    # /EtchServiceTier.etch#L117
-    if r_data["State"] > 8:
+    if state > 8:
         print(f"The job has finished with state {state}")
         break
     print(f"Waiting for job {job_id} to finish... current state: {state}")
     time.sleep(30)
 
 
-print("Fetching logs...")
-ft = hp.api.FileTransferApi(api_instance)
+
+print("\nFetching generated files...")
 ft_body = {
     "_preload_content": False,
     "body": {
@@ -145,55 +175,76 @@ ft_body = {
         "SessionCode": session_code
     }
 }
-r = ft.heappe_file_transfer_get_file_transfer_method_post(**ft_body)
+
+ftEndpoint = hp.FileTransferApi(api_instance)
+r = ftEndpoint.heappe_file_transfer_request_file_transfer_post(**ft_body)
+jobtransfer = json.loads(r.data)
+#print(jobtransfer)
+
+lchjf_body = {
+    "_preload_content": False,
+    "session_code": session_code,
+    "submitted_job_info_id": job_id
+}
+
+r = ftEndpoint.heappe_file_transfer_list_changed_files_for_job_get(**lchjf_body)
 r_data = json.loads(r.data)
+producedFiles = [os.path.normpath(path["FileName"]) for path in r_data]
+print(f"Files changed during job execution: {producedFiles}")
 
-r = ft.heappe_file_transfer_list_changed_files_for_job_post(**ft_body)
-filenames = [os.path.normpath(path) for path in json.loads(r.data)]
-
-print(f"Files changed during job execution: {filenames}")
-
-print("Fetching the files...")
+print("\nFetching the files...")
 ssh = SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh_username = r_data["Credentials"]["UserName"]
-pkey_file = StringIO(r_data["Credentials"]["PrivateKey"])
-pkey = paramiko.rsakey.RSAKey.from_private_key(pkey_file)
-ssh.connect(r_data["ServerHostname"], username=ssh_username, pkey=pkey)
-base_path = r_data["SharedBasepath"]
+pkey_file = StringIO(jobtransfer["Credentials"]["PrivateKey"])
+match jobtransfer["FileTransferCipherType"]:
+    case 1:
+        pkey = paramiko.RSAKey.from_private_key(pkey_file)
+    case 2:
+        pkey = paramiko.RSAKey.from_private_key(pkey_file)
+    case 3:
+        pkey = paramiko.ECDSAKey.from_private_key(pkey_file)
+    case 4:
+        pkey = paramiko.ECDSAKey.from_private_key(pkey_file)       
+    case default:
+        pkey = paramiko.RSAKey.from_private_key(pkey_file)
+
+ssh.connect(jobtransfer["ServerHostname"], username=jobtransfer["Credentials"]["Username"], pkey=pkey)
+base_path = jobtransfer["SharedBasepath"]
 with SCPClient(ssh.get_transport()) as scp:
-    for fn in filenames:
-        Path(os.path.dirname(fn)).mkdir(parents=True, exist_ok=True)
-        scp.get(os.path.join(base_path, fn), fn)
+    for fn in producedFiles:
+        Path(os.path.dirname(f"data_transfer/output/{job_id}/{fn}")).mkdir(parents=True, exist_ok=True)
+        scp.get(os.path.join(base_path, fn), f"data_transfer/output/{job_id}/{fn}")
+ssh.close()
 
 ft_body = {
     "_preload_content": False,
     "body": {
         "SubmittedJobInfoId": job_id,
-        "UsedTransferMethod": r_data,
+        "PublicKey": jobtransfer["Credentials"]["PublicKey"],
         "SessionCode": session_code
     }
 }
-r = ft.heappe_file_transfer_end_file_transfer_post(**ft_body)
+
+r = ftEndpoint.heappe_file_transfer_close_file_transfer_post(**ft_body)
 r_data = json.loads(r.data)
-print(", ".join(filenames) + " fetched")
+print(", ".join(producedFiles) + " fetched")
 
 
-print("Fetching resource usage report...")
-jr = hp.api.JobReportingApi(api_instance)
+print("\nFetching resource usage report...")
 rur_body = {
     "_preload_content": False,
-    "body": {
-        "JobId": job_id,
-        "SessionCode": session_code
-    }
+    "session_code": session_code,
+    "job_id": job_id
 }
-r = jr.heappe_job_reporting_get_resource_usage_report_for_job_post(**rur_body)
-r_data = json.loads(r.data)
-print("Done.")
 
-# Remove job after execution on HPC Cluster
-print(f"Removing job {job_id}...")
+jrEndpoint = hp.JobReportingApi(api_instance)
+r = jrEndpoint.heappe_job_reporting_resource_usage_report_for_job_get(**rur_body)
+r_data = json.loads(r.data)
+usedResources = r_data["TotalUsage"]
+print(f"Job {job_id} used {usedResources} resources...")
+
+
+print(f"\nRemoving job {job_id}...")
 ft_body = {
     "_preload_content": False,
     "body": {
@@ -201,6 +252,7 @@ ft_body = {
         "SessionCode": session_code
     }
 }
-r = jm.heappe_job_management_delete_job_post(**ft_body)
+
+r = jmEndpoint.heappe_job_management_delete_job_delete(**ft_body)
 r_data = json.loads(r.data)
 print(r_data)
