fdbdac25adf2a852b4b817d4253e59e4b927f492
Jan Swiatkowski <jan.swiatkowski@vsb.cz>
2023-07-26 09:47:56 +0200

    pyproject, setup, package publish

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
 from setuptools import setup, find_packages
 
-
-with open("requirements.txt") as reqs:
-    requirements = [line for line in reqs.read().split("\n") if line]
-
-
 setup(name="heappeac",
-      version="4.0",
-      description="HEAppE API Python client",
-      long_description="",
-      url="",
-      author="IT4I",
-      author_email="support.heappe@it4i.cz",
-      license="MIT",
-      packages=find_packages(),
-      install_requires=requirements)
+      packages=find_packages())
